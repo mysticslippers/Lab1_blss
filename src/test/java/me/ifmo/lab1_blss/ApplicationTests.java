65a4335def718656e4e6a9420bc18751192b076f
misticslippers <eternal.martyr13579@gmail.com>
2026-02-23 10:55:07 +0300

    update Application

--- src/test/java/me/ifmo/lab1_blss/ApplicationTests.java
+++ src/test/java/me/ifmo/lab1_blss/ApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab1BlssApplicationTests {
+class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
