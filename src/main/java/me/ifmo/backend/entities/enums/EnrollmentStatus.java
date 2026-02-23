ea6b5aa6b82586b3a00ab1c33c8d72fcab7814c7
misticslippers <eternal.martyr13579@gmail.com>
2026-02-23 11:04:07 +0300

    add EnrollmentStatus

---
+++ src/main/java/me/ifmo/backend/entities/enums/EnrollmentStatus.java
@@ -0,0 +1,12 @@
+package me.ifmo.backend.entities.enums;
+
+public enum EnrollmentStatus {
+    NEW,
+    REJECTED,
+    PENDING_PAYMENT,
+    ENROLLED,
+    PAYMENT_FAILED,
+    EXPIRED,
+    CANCELED,
+    REFUNDED
+}
