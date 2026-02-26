2aeb3462c4acaafa635379d8cd29b8aa004d3dce
misticslippers <eternal.martyr13579@gmail.com>
2026-02-26 20:11:57 +0300

    add PaymentIdMapper

---
+++ src/main/java/me/ifmo/backend/mappers/PaymentIdMapper.java
@@ -0,0 +1,19 @@
+package me.ifmo.backend.mappers;
+
+import me.ifmo.backend.entities.Payment;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PaymentIdMapper {
+
+    default Long toId(Payment payment) {
+        return payment == null ? null : payment.getId();
+    }
+
+    default Payment fromId(Long id) {
+        if (id == null) return null;
+        Payment p = new Payment();
+        p.setId(id);
+        return p;
+    }
+}
