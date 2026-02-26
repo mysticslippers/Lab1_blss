bcc6cf48923264d60e0f258a7e950ad13e1732fb
misticslippers <eternal.martyr13579@gmail.com>
2026-02-26 20:12:26 +0300

    update RefundMapper

--- src/main/java/me/ifmo/backend/mappers/RefundMapper.java
+++ src/main/java/me/ifmo/backend/mappers/RefundMapper.java
@@ -1,26 +1,18 @@
 package me.ifmo.backend.mappers;
 
 import me.ifmo.backend.DTO.refund.RefundDTO;
-import me.ifmo.backend.entities.Payment;
 import me.ifmo.backend.entities.Refund;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {PaymentIdMapper.class})
 public interface RefundMapper {
 
-    @Mapping(target = "paymentId", source = "payment.id")
+    @Mapping(target = "paymentId", source = "payment")
     RefundDTO toDto(Refund refund);
 
-    @Mapping(target = "payment", expression = "java(paymentFromId(dto.getPaymentId()))")
+    @Mapping(target = "payment", source = "paymentId")
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     Refund toEntity(RefundDTO dto);
-
-    default Payment paymentFromId(Long id) {
-        if (id == null) return null;
-        Payment payment = new Payment();
-        payment.setId(id);
-        return payment;
-    }
 }
