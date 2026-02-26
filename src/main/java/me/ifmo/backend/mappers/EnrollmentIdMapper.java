325d58309df1cc284da04f0b1aa6055096b3b4cf
misticslippers <eternal.martyr13579@gmail.com>
2026-02-26 20:11:24 +0300

    add EnrollmentIdMapper

---
+++ src/main/java/me/ifmo/backend/mappers/EnrollmentIdMapper.java
@@ -0,0 +1,19 @@
+package me.ifmo.backend.mappers;
+
+import me.ifmo.backend.entities.Enrollment;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface EnrollmentIdMapper {
+
+    default Long toId(Enrollment enrollment) {
+        return enrollment == null ? null : enrollment.getId();
+    }
+
+    default Enrollment fromId(Long id) {
+        if (id == null) return null;
+        Enrollment enrollment = new Enrollment();
+        enrollment.setId(id);
+        return enrollment;
+    }
+}
