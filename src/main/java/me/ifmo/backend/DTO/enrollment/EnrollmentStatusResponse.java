4f4d7c50c9c9fb7a47ba97b6ae5c5c7b84189ec3
misticslippers <eternal.martyr13579@gmail.com>
2026-02-26 19:59:44 +0300

    add EnrollmentStatusResponse

---
+++ src/main/java/me/ifmo/backend/DTO/enrollment/EnrollmentStatusResponse.java
@@ -0,0 +1,29 @@
+package me.ifmo.backend.DTO.enrollment;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import me.ifmo.backend.entities.enums.AccessStatus;
+import me.ifmo.backend.entities.enums.EnrollmentStatus;
+import me.ifmo.backend.entities.enums.PaymentStatus;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EnrollmentStatusResponse {
+
+    @NotNull(message = "EnrollmentStatusResponse.enrollmentId must not be null")
+    private Long enrollmentId;
+
+    @NotNull(message = "EnrollmentStatusResponse.enrollmentStatus must not be null")
+    private EnrollmentStatus enrollmentStatus;
+
+    @NotNull(message = "EnrollmentStatusResponse.paymentStatus must not be null")
+    private PaymentStatus paymentStatus;
+
+    @NotNull(message = "EnrollmentStatusResponse.accessStatus must not be null")
+    private AccessStatus accessStatus;
+}
