e6eee27b08273cd301ef58e492d760ee1ba5a1b1
misticslippers <eternal.martyr13579@gmail.com>
2026-03-01 19:26:24 +0300

    add User

---
+++ src/main/java/me/ifmo/backend/entities/User.java
@@ -0,0 +1,32 @@
+package me.ifmo.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false, insertable = false, updatable = false)
+    private LocalDateTime updatedAt;
+}
